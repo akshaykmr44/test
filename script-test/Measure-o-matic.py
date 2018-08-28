9fea7722023747dec870ea6cb890d468b8f68deb
AKSHAY KUMAR <42616719+akshaykmr44@users.noreply.github.com>
2018-08-28 17:48:56 +0530

    Delete Measure-o-matic.py

--- script-test/Measure-o-matic.py
+++
@@ -1,7 +0,0 @@
-import  sys
-import  os
-filename = input("Enter the path of your file: ")
-if os.path.isdir(filename):
-    f = open(filename,'r+')
-print(max(open(filename, 'r'), key=len))
-print(len(max(open(filename, 'r'), key=len)))
